7b102dbde7ebbd0e3b0c3bef101b1efb221b590f
wachin <wachin.id@gmail.com>
2024-10-07 08:19:54 -0500

    udpate

--- venv_launcher.sh
+++ venv_launcher.sh
@@ -1,10 +1,13 @@
 #! /bin/bash
 
+# Si su programa en python usa algun paquete desde
+# pip, esta es la manera de lanzarlo rapidamente
+# Pon este script en cualquier lugar donde está
+# programa en python
 # Activate the virtual environment
 source $HOME/.venv/bin/activate
 
-# Change to the directory containing the Python script
-cd .venv
-
+# Automáticamente se redigirá a este lugar desde
+# la activación de .venv
 # Run the Python script
 python3 script3.py
